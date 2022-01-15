e446955b59bdeccfebc00dcf108d1a5f6e482ece
Taufik Hidayat <97732208+taufikhdyat@users.noreply.github.com>
2022-01-15 16:06:19 +0700

    membuat static method dan class method
    membuat dan memahami static method dan class method

--- main.py
+++ main.py
@@ -4,15 +4,29 @@ class Hero :
     __jumlah = 0
 
     # encapsulasi : membuat semua variable menjadi private supaya nantinya menggunakan getter dan setter
-    def __init__(self, nama, health, attack, armor) :
-        self.__nama = nama
+    # getter dan setter diperlukan supaya atribut dapat diakses
+    def __init__(self, name, health, attack, armor) :
+        self.__name = name
         self.__health = health
         self.__attack = attack
         self.__armor = armor 
+        self.info = "name : {}  \n\t health : {}".format(self.__name,self.__health)
         Hero.__jumlah += 1
-  
 
-class Warrior(Hero) : # inherintance
+    
+        
+    #method static (decorator)
+    @staticmethod # tidak mengambil argumen
+    def getJumlah() : 
+        return Hero.__jumlah
+
+    @classmethod  # mengambil argumen
+    def getJumlah1(self):
+        return self.__jumlah
+
+
+# inherintance
+class Warrior(Hero) : 
     pass
 
 class Mage(Hero) :
@@ -22,6 +36,7 @@ eudora = Hero("eudora", 100, 7, 3)
 zilong = Warrior("zilong", 100, 8.5 , 7)
 miya = Mage("miya",100, 6.8, 6)
 
-print(eudora.__dict__)
-
-
+#print(miya.__name) # ini akan error, karena atributnya private
+print(zilong.info)
+#test jumlah
+print(Hero.getJumlah())
