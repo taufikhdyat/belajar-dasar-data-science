0c27cef7af05913dd692d96efbfc23d815dc3d75
Taufik Hidayat <97732208+taufikhdyat@users.noreply.github.com>
2022-01-15 11:56:02 +0700

    belajar OOP dasar

---
+++ main.py
@@ -0,0 +1,27 @@
+# belajar OOP
+
+class Hero :
+    __jumlah = 0
+
+    # encapsulasi : membuat semua variable menjadi private supaya nantinya menggunakan getter dan setter
+    def __init__(self, nama, health, attack, armor) :
+        self.__nama = nama
+        self.__health = health
+        self.__attack = attack
+        self.__armor = armor 
+        
+  
+
+class Warrior(Hero) : # inherintance
+    pass
+
+class Mage(Hero) :
+    pass
+
+eudora = Hero("eudora", 100, 7, 3)
+zilong = Warrior("zilong", 100, 8.5 , 7)
+miya = Mage("miya",100, 6.8, 6)
+
+print(eudora.__dict__)
+
+
